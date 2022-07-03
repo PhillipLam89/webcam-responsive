00941a56168f57c9dbd1c4dbe4d4857378d9207e
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2022-07-02 23:25:33 -0700

    now displays user camera brand/model

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let CONTEXT = null
 let SCALER = 0.9 //how much of total width we want the video to cover?
 
 const switchCameraButton = document.querySelector('#flip-camera')
-
+const streamDeviceInfo = document.querySelector('#streamDeviceInfo')
 let SIZE =  {x:0,y:0,width:0, height:0}
 let isCameraFacingUser = true
 let CURRENT_SIGNAL = null //global needed to store previous signal since we must STOP all media tracks before switching betweem cameras
@@ -19,7 +19,11 @@ function main() {
      VIDEO = document.createElement('video')
 
 
+
+     console.log(signal.getTracks())
      CURRENT_SIGNAL = signal
+     const tracks = CURRENT_SIGNAL.getTracks()
+    streamDeviceInfo.textContent = tracks[0].label.slice(0, tracks[0].label.indexOf('('))
      VIDEO.srcObject = signal
      VIDEO.setAttribute('autoplay', '');
      VIDEO.setAttribute('muted', '');
@@ -46,10 +50,10 @@ switchCameraButton.addEventListener('click', function() {
     promise.then(function(newSignal) {
       CURRENT_SIGNAL = newSignal
       VIDEO.srcObject = newSignal
-      VIDEO.setAttribute('autoplay', '');
-      VIDEO.setAttribute('muted', '');
+      // VIDEO.setAttribute('autoplay', '');
+      // VIDEO.setAttribute('muted', '');
 
-      VIDEO.setAttribute('playsinline', '')
+      // VIDEO.setAttribute('playsinline', '')
       VIDEO.play()
 
       VIDEO.onloadeddata = function() {
