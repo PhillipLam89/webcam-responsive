e783ef6efcaaf5db1a96860061e385999cadffe8
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2022-07-02 22:21:02 -0700

    fixed back camera access

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ let CANVAS = null
 let CONTEXT = null
 let SCALER = 0.9 //how much of total width we want the video to cover?
 
+const switchCameraButton = document.querySelector('#flip-camera')
+
 let SIZE =  {x:0,y:0,width:0, height:0}
-let cameraUserFacing = true
-const cameraFlipButton = document.querySelector('#flip-camera')
+let isCameraFacingUser = true
+let CURRENT_SIGNAL = null
 
 function main() {
   CANVAS = document.querySelector('#myCanvas')
@@ -15,81 +17,62 @@ function main() {
   let promise = navigator.mediaDevices.getUserMedia({video: true})
   promise.then(function(signal) {
      VIDEO = document.createElement('video')
+     CURRENT_SIGNAL = signal
      VIDEO.srcObject = signal
-     VIDEO.setAttribute('autoplay', ''); //will start streaming from video cam, REQUIRED for iOS
-     VIDEO.setAttribute('muted', ''); // REQUIRED for iOS
-     VIDEO.setAttribute('playsinline', '') //REQUIRED for iOS
+     VIDEO.setAttribute('autoplay', '');
+     VIDEO.setAttribute('muted', '');
+     VIDEO.setAttribute('playsinline', '')
      VIDEO.play()
+
      VIDEO.onloadeddata = function() {
-      handleResize()
-      window.addEventListener('resize', handleResize)
-          // we call handleResize here to to correctly modify our SIZE obj before rendering w/ updateCanvas()
-    // const videoStream = navigator.mediaDevices.getUserMedia({video: true})
-    // const videoTracks = videoStream.getTracks()
-    // videoTracks.forEach(track => track.stop())
-
-
+         handleResize()
+         window.addEventListener('resize', handleResize)
         updateCanvas()
-
-
-
      }
-
-
   }).catch(function(err) {
     alert('camera error dude' + err)
   })
 }
 
 
+switchCameraButton.addEventListener('click', function() {
+  isCameraFacingUser = !isCameraFacingUser
+  const tracks = CURRENT_SIGNAL.getTracks()
+  tracks.forEach(track => track.stop())
+  promise = navigator.mediaDevices.getUserMedia({video: { facingMode: `${isCameraFacingUser ? 'user' : 'environment'}`}})
+    promise.then(function(newSignal) {
+      CURRENT_SIGNAL = newSignal
+      VIDEO.srcObject = newSignal
+      VIDEO.setAttribute('autoplay', '');
+     VIDEO.setAttribute('muted', '');
+     VIDEO.setAttribute('playsinline', '')
+     VIDEO.play()
+
+     VIDEO.onloadeddata = function() {
+         handleResize()
+         window.addEventListener('resize', handleResize)
+        updateCanvas()
+     }
+    })
+})
+
 function handleResize() {
     CANVAS.width = window.innerWidth
     CANVAS.height = window.innerHeight
 
-      let resizerRatio = SCALER * Math.min(innerWidth / VIDEO.videoWidth, innerHeight / VIDEO.videoHeight)
+      let resizerRatio = SCALER * Math.min(innerWidth/VIDEO.videoWidth, innerHeight/VIDEO.videoHeight)
     SIZE.width = resizerRatio * VIDEO.videoWidth //preserves aspect ratio
     SIZE.height = resizerRatio * VIDEO.videoHeight
 
     SIZE.x = innerWidth / 2 - SIZE.width / 2   //starts at middle of canvas and shifts half its width to left
     SIZE.y = innerHeight / 2 - SIZE.height / 2
-
       //starts at middle of canvas and shifts half its height to top
 }
 
 function updateCanvas() {
-    CANVAS.style.background = `royalblue`
   CONTEXT.drawImage(VIDEO, SIZE.x, SIZE.y, SIZE.width, SIZE.height )
-
 
 
   requestAnimationFrame(updateCanvas) //will calls this function recursively (60 FPS if possible)
                                       // which will allow live stream video updates
 }
-
-
-//       cameraFlipButton.addEventListener('click', function () {
-//         cameraUserFacing = !cameraUserFacing
-//         const tracks = signal.getTracks()
-
-//         tracks.forEach(track => track.stop())
-//         promise = navigator.mediaDevices.getUserMedia({video: { facingMode: `${cameraUserFacing ? 'user' : 'environment'}`}})
-//           promise.then(function(newSignal) {
-//         VIDEO = document.createElement('video')
-//         VIDEO.id = 'newVideo'
-//         VIDEO.srcObject = newSignal
-//         VIDEO.setAttribute('autoplay', ''); //will start streaming from video cam, REQUIRED for iOS
-//         VIDEO.setAttribute('muted', ''); // REQUIRED for iOS
-//         VIDEO.setAttribute('playsinline', '') //REQUIRED for iOS
-
-
-//          VIDEO.onloadeddata = function() {
-
-//          handleResize() // we call handleResize here to to correctly modify our SIZE obj before rendering w/ updateCanvas()
-//     // const videoStream = navigator.mediaDevices.getUserMedia({video: true})
-//     // const videoTracks = videoStream.getTracks()
-//     // videoTracks.forEach(track => track.stop())
-//         updateCanvas()
-//      }
-
-//       } )
-// })
