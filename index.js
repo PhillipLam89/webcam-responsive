36022ee1effaf0c0ff91e1ed57e1e79dd62a153b
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2022-07-02 20:46:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,26 +4,62 @@ let CONTEXT = null
 let SCALER = 0.9 //how much of total width we want the video to cover?
 
 let SIZE =  {x:0,y:0,width:0, height:0}
-
+let cameraUserFacing = true
+const cameraFlipButton = document.querySelector('#flip-camera')
+let promise = null
 function main() {
   CANVAS = document.querySelector('#myCanvas')
   CONTEXT = CANVAS.getContext('2d')
 
 
-  let promise = navigator.mediaDevices.getUserMedia({video: { facingMode: 'environment'}})
+  promise = navigator.mediaDevices.getUserMedia({video: true, facingMode: 'user'})
   promise.then(function(signal) {
      VIDEO = document.createElement('video')
      VIDEO.srcObject = signal
-     VIDEO.setAttribute('autoplay', '');
-     VIDEO.setAttribute('muted', '');
-     VIDEO.setAttribute('playsinline', '')
-     VIDEO.play()
+     VIDEO.setAttribute('autoplay', ''); //will start streaming from video cam, REQUIRED for iOS
+     VIDEO.setAttribute('muted', ''); // REQUIRED for iOS
+     VIDEO.setAttribute('playsinline', '') //REQUIRED for iOS
 
      VIDEO.onloadeddata = function() {
-         handleResize()
+      handleResize()
          window.addEventListener('resize', handleResize)
+          // we call handleResize here to to correctly modify our SIZE obj before rendering w/ updateCanvas()
+    // const videoStream = navigator.mediaDevices.getUserMedia({video: true})
+    // const videoTracks = videoStream.getTracks()
+    // videoTracks.forEach(track => track.stop())
+
+
+        updateCanvas()
+
+
+
+     }
+      cameraFlipButton.addEventListener('click', function () {
+        cameraUserFacing = !cameraUserFacing
+        const tracks = signal.getTracks()
+
+        tracks.forEach(track => track.stop())
+        let newpromise = navigator.mediaDevices.getUserMedia({video: { facingMode: `${cameraUserFacing ? 'environment' : 'user'}`}})
+        newpromise.then(function(newSignal) {
+        VIDEO.srcObject = newSignal
+        // VIDEO.setAttribute('autoplay', ''); //will start streaming from video cam, REQUIRED for iOS
+        // VIDEO.setAttribute('muted', ''); // REQUIRED for iOS
+        // VIDEO.setAttribute('playsinline', '') //REQUIRED for iOS
+
+
+         VIDEO.onloadeddata = function() {
+         handleResize() // we call handleResize here to to correctly modify our SIZE obj before rendering w/ updateCanvas()
+    // const videoStream = navigator.mediaDevices.getUserMedia({video: true})
+    // const videoTracks = videoStream.getTracks()
+    // videoTracks.forEach(track => track.stop())
+
+
         updateCanvas()
      }
+
+      } )
+})
+
   }).catch(function(err) {
     alert('camera error dude' + err)
   })
@@ -34,19 +70,33 @@ function handleResize() {
     CANVAS.width = window.innerWidth
     CANVAS.height = window.innerHeight
 
-      let resizerRatio = SCALER * Math.min(innerWidth/VIDEO.videoWidth, innerHeight/VIDEO.videoHeight)
+      let resizerRatio = SCALER * Math.min(innerWidth / VIDEO.videoWidth, innerHeight / VIDEO.videoHeight)
     SIZE.width = resizerRatio * VIDEO.videoWidth //preserves aspect ratio
     SIZE.height = resizerRatio * VIDEO.videoHeight
 
     SIZE.x = innerWidth / 2 - SIZE.width / 2   //starts at middle of canvas and shifts half its width to left
     SIZE.y = innerHeight / 2 - SIZE.height / 2
+
       //starts at middle of canvas and shifts half its height to top
 }
 
 function updateCanvas() {
+    CANVAS.style.background = `royalblue`
   CONTEXT.drawImage(VIDEO, SIZE.x, SIZE.y, SIZE.width, SIZE.height )
+
 
 
   requestAnimationFrame(updateCanvas) //will calls this function recursively (60 FPS if possible)
                                       // which will allow live stream video updates
 }
+
+
+
+// cameraFlipButton.addEventListener('click', function() {
+//   cameraUserFacing = !cameraUserFacing
+
+//     const videoStream = navigator.mediaDevices.getUserMedia({video: true})
+//     const videoTracks = videoStream.getTracks()
+//     videoTracks.forEach(track => track.stop())
+
+// })
