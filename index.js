50d3e2139ccb52790e5196d152e3c6c50e20c6fd
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2022-07-09 19:39:40 -0700

    moar stylesz

--- index.js
+++ index.js
@@ -1,117 +1,66 @@
-let VIDEO = null  //all-caps elements mean they're GLOBAL
-let CANVAS = null
-let CONTEXT = null
-let SCALER = 0.9 //how much of total width we want the video to cover?
-
-
-const cameraShotSound = new Audio('./camera-sound.mp3')
-const videoRecordingSound = new Audio('./camera-record.mp3')
-const switchCameraButton = document.querySelector('#flip-camera')
-const streamDeviceInfo = document.querySelector('#streamDeviceInfo > h6')
-const SIZE =  {x:0,y:0,width:0, height:0}
-
-let isCameraFacingUser = true
-let CURRENT_SIGNAL = null //global needed to store previous signal since we must STOP all media tracks before switching betweem cameras
-let animateFrames = null
-let isVideoModeOn = true
-
-const takePhotoButton = document.querySelector('.take-photo')
-const savePhotoButton = document.querySelector('.save-photo')
-
-function main() {
-  CANVAS = document.querySelector('.myCanvas')
-  CONTEXT = CANVAS.getContext('2d')
-
-  let promise = navigator.mediaDevices.getUserMedia({video: true})
-  promise.then(function(signal) {
-     VIDEO = document.createElement('video')
-     CURRENT_SIGNAL = signal
-     const tracks = CURRENT_SIGNAL.getTracks()
-    streamDeviceInfo.textContent = tracks[0].label
-
-
-     VIDEO.srcObject = signal
-     VIDEO.setAttribute('autoplay', '')  //these 3 lines are needed for iPhones to work
-     VIDEO.setAttribute('muted', '') //these 3 lines are needed for iPhones to work
-     VIDEO.setAttribute('playsinline', '') //these 3 lines are needed for iPhones to work
-     VIDEO.play() // play method MUST be called for mobile to work, desktop isn't neccessary
-
-     VIDEO.onloadeddata = function() {
-
-         handleResize()
-         window.addEventListener('resize', handleResize)
-        updateCanvas()
-     }
-  }).catch(function(err) {
-    alert('camera error dude: ' + err)
-  })
+*{
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
 }
 
-
-switchCameraButton.addEventListener('click', function() {
-  isCameraFacingUser = !isCameraFacingUser
-  const tracks = CURRENT_SIGNAL.getTracks()
-  tracks.forEach(track => track.stop()) //we must stop previous video tracks to start a new one (aka switch from front/back cam)
-  promise = navigator.mediaDevices.getUserMedia({video: { facingMode: `${isCameraFacingUser ? 'user' : 'environment'}`}})
-    promise.then(function(newSignal) {
-      CURRENT_SIGNAL = newSignal
-      VIDEO.srcObject = newSignal
-      streamDeviceInfo.textContent = tracks[0].label
-      VIDEO.play()
-      VIDEO.onloadeddata = function() {
-         handleResize()
-         window.addEventListener('resize', handleResize)
-        //  updateCanvas()  we do NOT need to call updateCanvas() since VIDEO is already calling it previously
-     }
-    })
-})
-
-function handleResize() {
-    CANVAS.width = window.innerWidth
-    CANVAS.height = window.innerHeight
-
-      let resizerRatio = SCALER * Math.min(innerWidth/VIDEO.videoWidth, innerHeight/VIDEO.videoHeight)
-    SIZE.width = resizerRatio * VIDEO.videoWidth //preserves aspect ratio
-    SIZE.height = resizerRatio * VIDEO.videoHeight
-
-    SIZE.x = innerWidth / 2 - SIZE.width / 2   //starts at middle of canvas and shifts half its width to left
-    SIZE.y = innerHeight / 2 - SIZE.height / 2
-
-      //starts at middle of canvas and shifts half its height to top
+.hidden {
+    display: none !important;
 }
 
-function updateCanvas() {
- CONTEXT.drawImage(VIDEO, SIZE.x, SIZE.y, SIZE.width, SIZE.height )
-  animateFrames = window.requestAnimationFrame(updateCanvas) //will call this function recursively (60 FPS if possible)
-                                      // which will allow live stream video updates
+button{
+    position: absolute;
+    height: 30px;
+    left: 50%;
+    top: 15px;
+    padding: 3.25px;
+    background: red;
+    transform: translateX(-50%);
+
+
+}
+h6 {
+    width: 120px;
+    color: chartreuse;
+    font-size: 1rem;
+    font-weight: 900;
+}
+#streamDeviceInfo {
+
+    position: absolute;
+    top: 10%;
+    left: 45%;
+    width: 100%;
+
+   z-index: 3;
+}
+.take-photo {
+    position: absolute;
+    bottom: 2%;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: forestgreen;
+    border-radius: 50%;
+    height: 75px;
+    width: 75px;
+    padding: .35em;
+    cursor: pointer;
+    color: white;
+    font-weight: 900;
+    display: grid;
+    place-items: center;
 }
 
-takePhotoButton.addEventListener('click', function() {
-  isVideoModeOn = !isVideoModeOn
+.save-photo {
+    position: absolute;
+    cursor: pointer;
+    bottom: 2.5%;
+    left: 65%;
+    color: white;
 
-  if (!isVideoModeOn) {
+    font-weight: 900;
+    transform: translateX(-50%);
+    background-color: red;
+    padding: .5em;
 
-    this.style.background = 'royalblue'
-    this.textContent = 'Video'
-    switchCameraButton.classList.add('hidden')
-    cancelAnimationFrame(animateFrames)
-    cameraShotSound.play()
-  }
-  else {
-    this.style.background = 'forestgreen'
-    this.textContent = 'Take Pic'
-    switchCameraButton.classList.remove('hidden')
-    videoRecordingSound.play()
-    setTimeout(() => requestAnimationFrame(updateCanvas), 1500)
-  }
-
-
-})
-savePhotoButton.addEventListener('click', function() {
-    let data = CANVAS.toDataURL('image/png', 1) //1 will give best image quality
-    let a = document.createElement("a")
-    a.href = data
-
-    a.download = "myScreenShot.png"
-    a.click() //simulates an HTML element being clicked!
-})
+}
